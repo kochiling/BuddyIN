d626012be0be92ebec7ad23b27802f9009cef3be
kochiling <p21013435@student.newinti.edu.my>
2024-08-06 00:26:16 +0800

    Log Out and Login is implemented. the profile image and name can be displayed.

--- app/src/main/java/com/cscorner/buddyin/LoginFragment.java
+++ app/src/main/java/com/cscorner/buddyin/LoginFragment.java
@@ -9,20 +9,42 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.util.Patterns;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.Button;
 import android.widget.FrameLayout;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Objects;
 
 public class LoginFragment extends Fragment {
 
     TextInputEditText emailinput,passwordinput;
     Button loginbtn,createAccbtn,forgetPasswordBtn;
     FrameLayout parent_framelayout;
+    ProgressBar progressBar;
+    TextInputLayout emailinputlayout,passwordinputlayout;
+    FirebaseAuth mAuth;
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if (currentUser != null) {
+            Intent intent = new Intent(getActivity(), HomeActivity.class);
+            startActivity(intent);
+            requireActivity().finish();
+        }
+    }
 
     @SuppressLint("MissingInflatedId")
     @Override
@@ -37,7 +59,10 @@ public class LoginFragment extends Fragment {
         createAccbtn = view.findViewById(R.id.createAccbtn);
         forgetPasswordBtn = view.findViewById(R.id.forgetPasswordbtn);
         parent_framelayout = getActivity().findViewById(R.id.parent_framelayout);
-
+        progressBar = view.findViewById(R.id.progressBar);
+        mAuth = FirebaseAuth.getInstance();
+        emailinputlayout = view.findViewById(R.id.emailinputlayout);
+        passwordinputlayout = view.findViewById(R.id.passwordinputlayout);
         return view;
     }
 
@@ -48,20 +73,72 @@ public class LoginFragment extends Fragment {
         createAccbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //setFragment(new SignUpFragment());
-                Intent intent = new Intent(requireActivity(), RegisterActivity.class);
-                startActivity(intent);
+                setFragment(new SignUpFragment());
+                //Intent intent = new Intent(requireActivity(), RegisterActivity.class);
+                //startActivity(intent);
             }
         });
 
         loginbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(requireActivity(), HomeActivity.class);
-                startActivity(intent);
+
+                progressBar.setVisibility(View.VISIBLE);
+                String email = Objects.requireNonNull(emailinput.getText()).toString().trim();
+                String password = Objects.requireNonNull(passwordinput.getText()).toString().trim();
+
+                if (!validateEmail(email) || !validatePassword(password) ) {
+                    progressBar.setVisibility(View.GONE);
+                    return;
+                }
+
+
+                mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(task -> {
+                    progressBar.setVisibility(View.GONE);
+                    if (task.isSuccessful()) {
+                        Toast.makeText(getActivity(), "Login Successful.", Toast.LENGTH_SHORT).show();
+                        Intent intent = new Intent(getActivity(), HomeActivity.class);
+                        startActivity(intent);
+                        getActivity().finish();
+                    } else {
+                        Toast.makeText(getActivity(), "Authentication failed.", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
             }
         });
 
+    }
+
+    private boolean validateEmail(String email) {
+        if (email.isEmpty()) {
+            emailinputlayout.setError("Email address is required!");
+            Toast.makeText(getActivity(), "Email address is required!", Toast.LENGTH_SHORT).show();
+            emailinput.requestFocus();
+            return false;
+        } else if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            emailinputlayout.setError("Email address is invalid!");
+            Toast.makeText(getActivity(), "Email address is invalid!", Toast.LENGTH_SHORT).show();
+            emailinput.requestFocus();
+            return false;
+        } else {
+            emailinputlayout.setError(null);
+            return true;
+        }
+    }
+
+
+
+    private boolean validatePassword(String password) {
+        if (password.isEmpty()) {
+            passwordinputlayout.setError("Password is required!");
+            Toast.makeText(getActivity(), "Password is required!", Toast.LENGTH_SHORT).show();
+            passwordinput.requestFocus();
+            return false;
+        } else {
+            passwordinputlayout.setError(null);
+            return true;
+        }
     }
 
 
